0f8a36138cafa5593cdf7773583bae9e4db81dc3
OSCAR\oscar <oscarpardo2006@gmail.com>
2026-01-09 09:19:07 +0100

    #

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function registrar(){
+    let mensaje = ''
+    if (document.getElementById('nombre_usuario').value.length < 3 || document.getElementById('nombre_usuario').value.length > 20){
+        mensaje = mensaje + ' el campo nombre no puede tener una longitud menor de 3 y menor de 20 caracteres'
+    } if(document.getElementById('edad').value < 18 ) {
+        mensaje = mensaje + ' eres menor de edad'
+    }
+
+    if (mensaje.length > 0){
+        alert(mensaje)
+    } else {
+        alert('Todo correcto')
+    }
+}
