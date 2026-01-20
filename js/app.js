a6bffe807586281bb7d0e0097772e07719766936
DAW11\DAW111 <sergioescobar0510@gmail.com>
2026-01-20 09:48:23 +0100

    #

--- js/app.js
+++ js/app.js
@@ -1,34 +1,33 @@
 function registrar(){
-    let mensaje = ''
-    let mensajeError = document.getElementById("mensajeError")
-    let nombre_usuario = document.getElementById("nombre").value
-    let objetivo = document.getElementById("objetivo").value
-    let edad = document.getElementById("edad").value
-    let apellidos_usuario = document.getElementById("apellidos").value
+   let mensaje = ''
+   let mensajeError = document.getElementById("mensajeError")
+   let nombre_usuario = document.getElementById("nombre").value
+   let objetivo = document.getElementById("objetivo").value
+   let edad = document.getElementById("edad").value
+   let apellidos_usuario = document.getElementById("apellidos").value
 
-     if(nombre_usuario.length<3 || nombre_usuario.length>20){
-        mensaje = mensaje + "<p class=mensajeError>'><strong>El nombre debe tener entre 3 y 20 caracteres</p>"
-     }     
-     if(objetivo.length<10 || objetivo.length>200){
-        mensaje = mensaje + "<p class=mensajeError>'><strong>El objetivo debe tener entre 10 y 200 caracteres</p>"
-     }          
-     if(edad<1 || edad>100){
-        mensaje = mensaje + "<p class=mensajeError>'><strong>La edad debe estar entre 1 y 120</p>"
-     }
-        if(apellidos_usuario.length<3 || apellidos_usuario.length>30){
-            mensaje = mensaje + "<p class='mensajeError'><strong>Los apellidos deben tener entre 3 y 30 caracteres</strong></p>"
-        }
-        
+   if(nombre_usuario.length<3 || nombre_usuario.length>20){
+      mensaje = mensaje + "<p class=mensajeError>'><strong>El nombre debe tener entre 3 y 20 caracteres</p>"
+   }     
+   if(objetivo.length<10 || objetivo.length>200){
+      mensaje = mensaje + "<p class=mensajeError>'><strong>El objetivo debe tener entre 10 y 200 caracteres</p>"
+   }          
+   if(edad<1 || edad>100){
+      mensaje = mensaje + "<p class=mensajeError>'><strong>La edad debe estar entre 1 y 120</p>"
+   }
+      if(apellidos_usuario.length<3 || apellidos_usuario.length>30){
+         mensaje = mensaje + "<p class='mensajeError'><strong>Los apellidos deben tener entre 3 y 30 caracteres</strong></p>"
+      }     
         mensajeError.innerHTML = mensaje
    }
             
 function modoOscuro() {
    document.body.classList.toggle('oscuro');
    if (document.body.classList.contains('oscuro')) {
-        document.getElementById('cambiarModo').src = '/img/modo-claro.png';
-    } else {
-        document.getElementById('cambiarModo').src = '/img/modo-oscuro.png';
-    }
+      document.getElementById('cambiarModo').src = '/img/modo-claro.png';
+   } else {
+      document.getElementById('cambiarModo').src = '/img/modo-oscuro.png';
+   }
 }
 
 /*function init(){
